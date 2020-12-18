38aa29070a627d02fe2ac5ebddd492c093fa6e82
warrenniu <warrenniu@gmail.com>
2020-12-17 22:33:39 -0500

    completed

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -5,6 +5,19 @@ const Actors = () => {
   return (
     <div>
       {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map((actor, index) => (
+
+        <div key={index}>
+          <h3>Name: {actor.name}</h3>
+          <p>Movies:</p>
+          <ul>
+            {actor.movies.map((actor, index) => (
+              <li key={index}>{actor}</li>
+            ))}
+          </ul>
+          </div>
+      ))}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,26 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
+
   return (
     <div>
-        {/*{code here}*/}
+      {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map((movie, index) => (
+
+
+        <div key={index}>
+          <h3>Movie: {movie.title}</h3>
+          <p>Time: {movie.time}</p>
+          <p>Genre:</p>
+          <ul>
+            {movie.genres.map((genre, index) => (
+              <li key={index}>{genre}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
